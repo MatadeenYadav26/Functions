0cc0b1cc7431182be07caa27e3306d97dae42935
KING <matadeenyadav202@gmail.com>
2025-12-24 20:26:17 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,26 @@
+# def add (a,b):
+#     print(a+b) 
+
+# add(2,5)
+
+#Pallindrome
+
+# def pallindrome (x):
+#     rev = 0
+#     copy = x
+#     while x > 0 :
+#         rev = (rev * 10) + (x % 10)
+#         x = x//10
+
+#     if rev == copy:
+#         return True
+#     else:
+#         return False
+    
+
+# print(pallindrome(1221))
+# print(pallindrome(1331))
+# print(pallindrome(1234))
+
+#Args and Kwargs
+
